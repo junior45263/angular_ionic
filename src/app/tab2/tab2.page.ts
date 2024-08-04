2725347e15c9ac30d34bbabd33c2f9256e50f92d
Junior Castañeda Rios <bc4363624@gmail.com>
2024-08-04 11:06:06 -0600

    v1

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { PhotoService } from './../services/photo.service';
+import { Component, OnInit } from '@angular/core';
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
+import { environment } from 'src/environments/environment';
+import { enableProdMode } from '@angular/core';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  styleUrls: ['tab2.page.scss'],
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
+  photos: any[] = [];
 
-  constructor() {}
+  constructor(public photoService: PhotoService) {
+    this.photos = this.photoService.photos;
+  }
 
+  ngOnInit() {
+    defineCustomElements(window);
+
+    if (environment.production) {
+      enableProdMode();
+    }
+  }
+
+  takePhoto() {
+    this.photoService.addPhoto();
+  }
 }
